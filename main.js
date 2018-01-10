eb1625442342c99e79af388e88d8ef6941f2e57b
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-01-09 22:55:19 -0500

    Pulling in user input

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 // Link in the Inquirer Package
 var inquirer = require('inquirer');
 
-inquirer.prompt([
+function playGame() {
+    inquirer.prompt([
     {
       type: "value",
       name: "letter",
-      message: "Guess a Letter: "
-    }]);
+      message: "Guess a Letter and hit enter: "
+    }
+    ]).then(function(userInput){
+
+    // Take in user input and make sure it's lowercase
+    var inputLetter = userInput.letter.toLowerCase();
+    console.log("You picked " + inputLetter);
+    })
+}
+
+playGame ();
