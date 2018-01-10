a7a499e90718e79a3e72c05249ac42aa1f6479bc
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-01-09 23:22:18 -0500

    Adding word list

---
+++ words.js
@@ -0,0 +1,37 @@
+// Game will randomly select a word from the list below.
+
+var WordsList = [
+    "baked alaska",
+    "baked apple", 
+    "banana split",
+    "waffle",
+    "bread pudding",
+    "brownie",
+    "butterscotch"
+    "marshmallow",
+    "milkshake",
+    "mousse",
+    "cake",
+    "cannoli"
+    "caramel apple",
+    "carrot cake",
+    "cheesecake",
+    "chocolate",
+    "churro",
+    "cobbler",
+    "cookie",
+    "crepe",
+    "cupcake",
+    "custard",
+    "gelatin",
+    "gelato",
+    "gingersnaps",
+    "gingerbread",
+    "honey",
+    "ice cream",
+    "jam",
+    "jelly",
+  ];
+  
+  // Export the list of words
+  module.exports = WordsList;
